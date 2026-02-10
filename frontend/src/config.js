8c73a06fb6d6a6bed9688e892ea50e647d12ef20
vishnukainskep <vishnuparihar239925@gmail.com>
2026-02-10 23:25:42 +0530

    base url

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,7 +1,8 @@
-const API_BASE_URL = import.meta.env.PROD 
-  ? 'https://vinitmalviya.onrender.com' 
-  : 'http://localhost:8000';
+// const API_BASE_URL = import.meta.env.PROD 
+//   ? 'https://vinitmalviya.onrender.com' 
+//   : 'http://localhost:8000';
 
+const API_BASE_URL = 'https://vinitmalviya.onrender.com';
 export default API_BASE_URL;
 
 export const getImageUrl = (url) => {
